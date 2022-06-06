b1e8b7d9196ebb9afa78d8965755d5c3bcebc796
dioripribadisinaga@gmail.com <dioripribadisinaga@gmail.com>
2022-06-06 15:46:17 +0700

    react

--- src/components/MulaiSewa.js
+++ src/components/MulaiSewa.js
@@ -3,7 +3,7 @@ import { Container,Button} from 'react-bootstrap'
 
 export default function MulaiSewa() {
     return (
-        <div className='my-4 '>
+        <div className='my-4' >
             <Container className='text-center text-white py-5' style={{background:'#0d28a6',borderRadius:'20px',marginLeft:'auto',marginRight:'auto',maxWidth:'90%'}}>
                 <h3 className='mb-4'>Sewa Mobil di (Lokasimu) Sekarang</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum temporibus quas vero, aut vitae debitis, iure sequi aliquam modi nam, eveniet repellat similique quam cupiditate neque quae consectetur eos est.</p>
